b4d973f80db414c10ccd42a6818eec5fb5cbe958
Boxvi <hermosokawai@gmail.com>
2022-11-02 07:44:34 -0500

    web service

---
+++ src/main/java/ec/edu/ista/springgc1/service/GenericService.java
@@ -0,0 +1,15 @@
+package ec.edu.ista.springgc1.service;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+public interface GenericService<T, ID> extends Serializable {
+
+    T save(T t);
+
+    Iterable<T> findAll();
+
+    void deleteById(ID id);
+
+    Optional<T> findById(ID id);
+}
